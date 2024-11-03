72cf629b8714fc6dbae5ddbe544635ed59b58474
Mike Maynard <mike@pointxero.co>
2024-11-03 23:08:48 +0200

    Testing of Client Server connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the newest version of the React App!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [greeting, setGreeting] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:5002/api/hello')
+            .then(response => {
+                setGreeting(response.data.msg);
+                console.log('Message fetched successfully:', response.data.msg);
+            })
+            .catch(error => {
+                console.error('Error fetching the message:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>{greeting}</h1>
+        </div>
+    );
 }
 
 export default App;
